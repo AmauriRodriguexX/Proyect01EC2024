4e18d087aab3897e2ce0a6880c74dbab74d2e372
AmauriRodriguexX <amauri.rodriguez@t2o.com>
2024-06-11 15:31:51 -0600

    modal mobile

---
+++ assets/js/modal-share.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const popupBottom = document.getElementById('popupBottom');
+    const overlay = document.getElementById('overlay');
+    const shareBtn = document.getElementById('shareButton');
+    const closeBtn = document.querySelector('.close-btn');
+    let startY;
+
+    shareBtn.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent default action
+        popupBottom.classList.add('show');
+        popupBottom.style.display = 'block'; // Ensure the modal is displayed
+        overlay.classList.add('show-overlay');
+        overlay.style.display = 'block'; // Ensure the overlay is displayed
+    });
+
+    closeBtn.addEventListener('click', () => {
+        closeModal();
+    });
+
+    overlay.addEventListener('click', () => {
+        closeModal();
+    });
+
+    popupBottom.addEventListener('touchstart', (event) => {
+        startY = event.touches[0].clientY;
+    });
+
+    popupBottom.addEventListener('touchmove', (event) => {
+        const moveY = event.touches[0].clientY;
+        const diffY = moveY - startY;
+        
+        if (diffY > 50) { // Adjust the threshold as needed
+            closeModal();
+        }
+    });
+
+    function closeModal() {
+        popupBottom.classList.remove('show');
+        overlay.classList.remove('show-overlay');
+        setTimeout(() => {
+            popupBottom.style.display = 'none'; // Hide the modal after animation
+            overlay.style.display = 'none'; // Hide the overlay after animation
+        }, 300);
+    }
+});
